4421e128e06ac13f173dee3aaf54935fe9d1f7b0
kyung01 <iopzxz@gmail.com>
2014-04-11 02:59:44 -0400

    level selector... need to be done please

---
+++ New Unity Project 1/Assets/00Scripts/Graphics/Interface/BehaviorFillScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class BehaviorFillScreen : EasyGameObject
+{
+    public float zDeapth = 5.0f;
+    void Awake()
+    {
+        Vector3 bottom = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, zDeapth)),
+                top = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, zDeapth)),
+                scale = top - bottom;
+        var parent = transform.parent;
+        transform.parent = null;
+
+        transform.localScale = new Vector3(Mathf.Abs(scale.x),Mathf.Abs( scale.y), 1);
+        transform.parent = parent;
+        //Camera.main.ScreenToWorldPoint()
+    }
+}
---
+++ New Unity Project 1/Assets/00Scripts/Graphics/Interface/IcnLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class IcnLevel : EasyGameObject
+{
+    EasyTextMesh icnLevel,icnProgress;
+    public void display(int level, int levelSize, int levelCompleted = 0)
+    {
+        icnLevel.display("" + level);
+        icnProgress.display("" + levelCompleted + " / " + levelSize);
+    }
+}
